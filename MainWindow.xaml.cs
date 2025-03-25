0f86d9c2e952460f13d40c5b5f1360081524a0d3
Ollox <axe999@gmx.de>
2025-03-25 16:31:42 +0100

    removed nullable warning

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.Text;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.Win32;
-using MessageBox = System.Windows.MessageBox;
 
 namespace LogFileViewer
 {
