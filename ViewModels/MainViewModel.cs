c3b2683b84b7554889c009b15fe3634690476ed7
Ollox <axe999@gmx.de>
2025-03-28 12:26:29 +0100

    feat(viewmodel): Implement folder selection functionality - Add SelectedFolderPath property with change notification - Implement SelectFolder method with FolderBrowserDialog - Add Windows Forms reference for dialog functionality

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -3,16 +3,42 @@ using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using System.Windows;
 
 namespace LogFileViewer
 {
     public class MainViewModel : PropertyChangedBase, IMain
     {
-        
+        private string _selectedFolderPath;
+        public string SelectedFolderPath
+        {
+            get => _selectedFolderPath;
+            set
+            {
+                _selectedFolderPath = value;
+                NotifyOfPropertyChange(() => SelectedFolderPath);
+            }
+        }
 
         public MainViewModel()
         {
             
+        }
+
+        public void SelectFolder()
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "Select a folder containing log files";
+                dialog.UseDescriptionForTitle = true; // This property is available in .NET 5+ for a better UX
+                
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    SelectedFolderPath = dialog.SelectedPath;
+                    // TODO: Load log files from the selected folder
+                }
+            }
         }
     }
 } 
